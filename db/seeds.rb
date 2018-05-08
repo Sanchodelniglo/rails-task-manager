962996e8775ac8d60dcd38ac2a5624486cd8494f
Romain Grossard <rgrossard@gmail.com>
2018-05-08 15:42:17 +0200

    exo1 chaud ma gueule

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(title: 'laundry', details: 'lorem ta mere')
+Task.create(title: 'bathroom', details: "récupérer les boucles d'oreilles de ma femme dans le syphon")
+Task.create(title: 'Whipmaking', details: "Découper la peau du Kangourou", completed: true)
+
