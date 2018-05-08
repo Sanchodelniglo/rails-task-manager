22fd6bde9a6a5638c2cf7bbe3f62ef76efdf69a0
Romain Grossard <rgrossard@gmail.com>
2018-05-08 16:08:05 +0200

    exo1 refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #  get    "restaurants",          to: "restaurants#index"
-
-  # get    "restaurants/new",      to: "restaurants#new"
-  # post   "restaurants",          to: "restaurants#create"
-
-  # # NB: The `show` route needs to be *after* `new` route.
-  # get    "restaurants/:id",      to: "restaurants#show"
-
-  # get    "restaurants/:id/edit", to: "restaurants#edit"
-  # patch  "restaurants/:id",      to: "restaurants#update"
-
-  # delete "restaurants/:id",      to: "restaurants#destroy"
-  get 'tasks', to: 'tasks#index', as: :tasks
-  get 'task/:id', to: 'tasks#show', as: :task
-
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create', as: :create_task
-
-  get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'task/:id', to: 'tasks#update', as: :update_task
-
-  delete 'task/:id', to: 'tasks#destroy', as: :delete_task
+  resources :tasks
 end
